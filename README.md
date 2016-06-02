f50385606b9a80f7dc2873df89a5eaeb8033b875
Ryan Leckey <leckey.ryan@gmail.com>
2016-06-02 01:16:09 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # go-develop
+> Wrapper for `go get` to setup a project for development.
+
+## Features
+ - Uses `git clone` (therefore supports ssh, private repositories, etc.)
+ - Operates like `git clone` by creating a symlink to the project directory from GOPATH to the current directory
+
+## Usage
+Each of the following commands result in an identical project layout in GOPATH.
+
+```
+$ go-develop https://github.com/mehcode/go-develop.git
+$ go-develop https://github.com/mehcode/go-develop
+$ go-develop git@github.com:mehcode/go-develop
+$ go-develop git@github.com:mehcode/go-develop.git
+$ go-develop ssh://git@github.com/mehcode/go-develop.git
+```
---
+++ index.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"net/url"
+	"os"
+	"os/exec"
+	"path"
+	"regexp"
+	"strings"
+)
+
+var sshShorthandR = regexp.MustCompile(`.*?\@(.*)$`)
+var extensionR = regexp.MustCompile(`(\..*)$`)
+
+type gitURL struct {
+	Source string
+	Owner  string
+	Name   string
+}
+
+func parse(text string) gitURL {
+	u, err := url.Parse(text)
+	if err != nil {
+		panic(err)
+	}
+
+	var result gitURL
+	path := u.Path
+
+	// Handle *@* for ssh shorthand
+	m := sshShorthandR.FindStringSubmatch(u.Path)
+	if len(m) >= 2 {
+		parts := strings.SplitN(m[1], ":", 2)
+		result.Source = parts[0]
+		path = parts[1]
+	} else {
+		// Assume it's a normal URL and just piecemeal
+		result.Source = u.Host
+	}
+
+	// Remove `.*` from path
+	path = extensionR.ReplaceAllString(path, "")
+
+	// Split path into owner and name
+	parts := strings.SplitN(path, "/", 2)
+	result.Owner = parts[0]
+	result.Name = parts[1]
+
+	return result
+}
+
+func main() {
+	// Parse the git URL from the command line
+	text := os.Args[1]
+	url := parse(text)
+
+	// Get the destination folder (for symlink)
+	var linkTarget string
+	if len(os.Args) >= 3 {
+		linkTarget = os.Args[2]
+	} else {
+		linkTarget = url.Name
+	}
+
+	// Get GOPATH
+	gopath := os.Getenv("GOPATH")
+
+	// Build the GOPATH clone target
+	cloneTarget := path.Join(gopath, "src", url.Source, url.Owner, url.Name)
+
+	// Clone the repository
+	cmd := exec.Command("git", "clone", text, cloneTarget)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	// Link the repository
+	err = os.Symlink(cloneTarget, linkTarget)
+	if err != nil {
+		panic(err)
+	}
+}
